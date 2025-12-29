6d816dced74798de931c6fe40d28f45f41ee6e12
Naresh <naresh@peernova.com>
2025-12-29 16:21:07 -0600

    added fix for delete to fetch latest commit per branch

--- src/metadata_manager/postgres_metadata_manager.cpp
+++ src/metadata_manager/postgres_metadata_manager.cpp
@@ -1,4 +1,6 @@
 #include "metadata_manager/postgres_metadata_manager.hpp"
+#include "storage/ducklake_catalog.hpp"
+#include "storage/ducklake_transaction.hpp"
 
 namespace duckdb {
 
@@ -19,13 +21,17 @@ bool PostgresMetadataManager::TypeIsNativelySupported(const LogicalType &type) {
 }
 
 string PostgresMetadataManager::GetLatestSnapshotQuery() const {
-	return R"(
+	auto &catalog = transaction.GetCatalog();
+	auto working_branch = catalog.GetWorkingBranch();
+
+	return StringUtil::Format(R"(
 	SELECT * FROM postgres_query({METADATA_CATALOG_NAME_LITERAL},
 		'SELECT snapshot_id, schema_version, next_catalog_id, next_file_id
-		 FROM {METADATA_SCHEMA_ESCAPED}.ducklake_snapshot WHERE snapshot_id = (
-		     SELECT MAX(snapshot_id) FROM {METADATA_SCHEMA_ESCAPED}.ducklake_snapshot
+		 FROM {METADATA_SCHEMA_ESCAPED}.ducklake_snapshot
+		 WHERE branch_id = %d AND snapshot_id = (
+		     SELECT MAX(snapshot_id) FROM {METADATA_SCHEMA_ESCAPED}.ducklake_snapshot WHERE branch_id = %d
 		 );')
-	)";
+	)", working_branch.index, working_branch.index);
 }
 
 } // namespace duckdb
